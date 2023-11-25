1c7696b10353d04c4af9ce839a82b8a861ee7304
Mats-Erik Eriksen <mder@equinor.com>
2023-11-25 15:55:41 +0100

    Simple mob XRay-vision fix

--- Mob.gd
+++ Mob.gd
@@ -6,26 +6,34 @@ signal squashed
 @export var view_distance: int = 8
 var viewCone: ShapeCast3D
 var path: PathFollow3D
+var patroll: bool
 
 func _ready():
 	viewCone = get_node("Pivot/Character/ShapeCast3D")
 	path = get_parent()
 
 func _physics_process(_delta):
-	
+	patroll = true
 	if viewCone.is_colliding():
-		var target = viewCone.get_collision_point(0)
-		look_at_from_position(
-			Vector3(self.global_position),
-			Vector3(target.x, self.position.y, target.z),
-			Vector3.UP)
-		#velocity = Vector3.FORWARD * speed
-		#velocity = velocity.rotated(Vector3.UP, rotation.y)
-		#move_and_slide()
-	else:
+		var collidedObject = viewCone.get_collider(0)
+		if collidedObject != null && collidedObject.name == "Player":
+			var target = viewCone.get_collision_point(0)
+			var space_state = get_world_3d().direct_space_state
+			var query = PhysicsRayQueryParameters3D.create(self.global_position, target)
+			query.collide_with_bodies = true
+			var result = space_state.intersect_ray(query)
+			if not result.is_empty() && result["collider"].name == "Player":
+				patroll=false
+				target.y = self.global_position.y
+				look_at_from_position(
+					Vector3(self.global_position),
+					Vector3(target),
+					Vector3.UP)
+	
+	if patroll:
 		path.progress += speed * _delta
 		if rotation_degrees != Vector3.ZERO:
-			rotation_degrees = lerp(rotation_degrees, Vector3.ZERO, 0.5) 
+			rotation_degrees = lerp(rotation_degrees, Vector3.ZERO, 0.5)
 		
 
 func squash():
